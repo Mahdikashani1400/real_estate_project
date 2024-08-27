762601fa762a8a00958f50ce94ecf33f018549a7
Mahdikashani1400 <mahdi.kashan96@gmail.com>
2024-08-28 01:16:16 +0330

    pagination,sortable,search features is developed

--- components/modules/HosesBox.js
+++ components/modules/HosesBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../../styles/house_box.module.css"
 import { useRouter } from 'next/router'
 import Link from 'next/link'
@@ -6,27 +6,68 @@ import db from "../../data/db.json"
 export default function HosesBox() {
     const route = useRouter()
     const basePath = route.asPath.split('/')[1]
-    const data = db.homes
+    const itemsOnPage = 3
+    const [data, setData] = useState([...db.homes])
+
+    const [search, setSearch] = useState('')
+    const [sort, setSort] = useState('choise')
+    const [page, setPage] = useState(1)
+
+    console.log([...data]);
+    console.log([...data].slice(3, 6));
+    useEffect(() => {
+        setData(prevState => [...db.homes].filter(house => house.title.includes(search)))
+    }, [search])
+    useEffect(() => {
+        switch (sort) {
+            case "choise": {
+                setData(prevState => [...db.homes].sort((house1, house2) => house1.id - house2.id))
+                break
+
+            }
+            case "price": {
+                setData(prevState => [...db.homes].sort((house1, house2) => house2.price - house1.price))
+                break
+            }
+            case "room": {
+                setData(prevState => [...db.homes].sort((house1, house2) => house2.roomCount - house1.roomCount))
+                break
+            }
+            case "address": {
+                setData(prevState => [...db.homes].sort((house1, house2) => house1.id - house2.id))
+                break
+            }
+            case "metr": {
+                setData(prevState => [...db.homes].sort((house1, house2) => house2.meterage - house1.meterage))
+                break
+            }
+
+            default:
+                break;
+        }
+    }, [sort])
     return (
         <>
             <div className={styles["home-section"]} id="houses">
                 {basePath === "houses" ? <div className={styles["home-filter-search"]}>
                     <div className={styles["home-filter"]}>
-                        <select name="" id="">
-                            <option value="" selected>انتخاب کنید</option>
-                            <option value="">بر اساس قیمت</option>
-                            <option value="">بر اساس تعداد اتاق</option>
-                            <option value="">بر اساس ادرس</option>
-                            <option value="">بر اساس اندازه</option>
+                        <select name="" id="" onChange={(e) => {
+                            setSort(prevState => e.target.value)
+                        }}>
+                            <option value="choise" selected>انتخاب کنید</option>
+                            <option value="price">بر اساس قیمت</option>
+                            <option value="room">بر اساس تعداد اتاق</option>
+                            <option value="address">بر اساس ادرس</option>
+                            <option value="metr">بر اساس اندازه</option>
                         </select>
                     </div>
                     <div className={styles["home-search"]}>
-                        <input type="text" placeholder="جستجو کنید" />
+                        <input type="text" placeholder="جستجو کنید" onChange={(e) => { setSearch(e.target.value) }} />
                     </div>
                 </div> : ""}
                 <div className={`${styles['homes']} ${basePath === "houses" ? "" : `${styles['single']}`}`}>
                     {
-                        data.map(house => <div className="card">
+                        data.length ? [...data].slice((page - 1) * itemsOnPage, page * itemsOnPage).map(house => <div className="card">
                             <img src={house.img} alt={`House ${house.id}`} className="card__img" />
                             <h5 className="card__title">{house.title}</h5>
                             <svg className="card__like">
@@ -68,7 +109,11 @@ export default function HosesBox() {
                                 // }
                                 href={`/houses/${house.code}`}
                                 className="btn btn-brown btn-card">مشاهده ملک</Link>
-                        </div>)
+                        </div>) : <>
+                            <div className={styles['search-error']}>
+                                خانه مد نظر شما یافت نشد ...
+                            </div>
+                        </>
                     }
 
 
@@ -76,9 +121,18 @@ export default function HosesBox() {
 
                 {
                     basePath === "houses" ? <ul className={styles["pagination__list"]}>
-                        <li className={styles["pagination__item"]}><a href="#" className=""> {"<"} </a></li>
-                        <li className={styles["pagination__item"]}><a href="#" className="">2</a></li>
-                        <li className={`${styles["pagination__item"]} ${styles["active"]}`}><a href="#" className="">1</a></li>
+                        <li className={styles["pagination__item"]} onClick={() => {
+                            setPage(prevState => {
+                                if (prevState === Math.round([...db.homes].length / itemsOnPage)) {
+                                    return 1
+                                } else {
+                                    return prevState + 1
+                                }
+                            })
+                        }}><span className=""> {"<"} </span></li>
+
+                        {Array.from({ length: Math.round([...db.homes].length / itemsOnPage) }).map((item, index) => <li className={`${index + 1 === page ? styles["active"] : ""} ${styles["pagination__item"]}`} onClick={() => { setPage(prevState => index + 1) }}><span className="">{index + 1}</span></li>)}
+
                     </ul> : ""
                 }
             </div>
--- components/templates/RealTors.js
+++ components/templates/RealTors.js
@@ -1,45 +1,76 @@
 import React from 'react'
 import styles from "../../styles/real_tors.module.css"
+import { useRouter } from 'next/router'
+import db from "../../data/db.json"
 
 export default function RealTors() {
+    const route = useRouter()
+    const basePath = route.asPath.split('/')[1]
+    const data = db.homes
+    console.log(data);
+
+    const expensiveHouses = [...data].sort((house1, house2) => house1.price - house2.price).slice(0, 3)
     return (
         <div className={styles["real-tors"]}>
-            <p className={styles["real-tors__tittle"]}>سه مالک برتر</p>
-            <div className={styles["real-tors__list"]}>
-                <img
-                    src="/img/realtor-1.jpeg"
-                    alt="real-tors top 1"
-                    className={styles["real-tors__img"]}
-                />
-                <div className={styles["real-tors__details"]}>
-                    <h3 className={"u-heading-3  u-heading--white"}>
-                        مهدی ایلخانی نسب
-                    </h3>
-                    <p className={styles["real-tors__text"]}>869 فروش خانه</p>
-                </div>
+            {
+                basePath === 'houses' ? <>
+                    <p className={styles["real-tors__tittle"]}>سه خانه ارزان</p>
+                    <div className={styles["real-tors__list"]}>
+                        {expensiveHouses.map(house => <>
+                            <img
+                                src={house.img}
+                                alt="real-tors top 1"
+                                className={styles["real-tors__img"]}
+                            />
+                            <div className={styles["real-tors__details"]}>
+                                <h3 className={"u-heading-3  u-heading--white"}>
+                                    {house.title}
+                                </h3>
+                                <p className={styles["real-tors__text"]}>{house.price.toLocaleString("en-US")},000,000 تومان</p>
+                            </div>
+                        </>)}
 
-                <img
-                    src="/img/realtor-2.jpeg"
-                    alt="real-tors top 2"
-                    className={styles["real-tors__img"]}
-                />
-                <div className={styles["real-tors__details"]}>
-                    <h3 className={"u-heading-3  u-heading--white"}>کوثر بهشتی</h3>
-                    <p className={styles["real-tors__text"]}>243 فروش خانه</p>
-                </div>
 
-                <img
-                    src="/img/realtor-3.jpeg"
-                    alt="real-tors top 3"
-                    className={styles["real-tors__img"]}
-                />
-                <div className={styles["real-tors__details"]}>
-                    <h3 className={"u-heading-3  u-heading--white"}>
-                        عرشیا احسنی
-                    </h3>
-                    <p className={styles["real-tors__text"]}>130 فروش خانه</p>
-                </div>
-            </div>
+                    </div>
+                </> : <>
+                    <p className={styles["real-tors__tittle"]}>سه مالک برتر</p>
+                    <div className={styles["real-tors__list"]}>
+                        <img
+                            src="/img/realtor-1.jpeg"
+                            alt="real-tors top 1"
+                            className={styles["real-tors__img"]}
+                        />
+                        <div className={styles["real-tors__details"]}>
+                            <h3 className={"u-heading-3  u-heading--white"}>
+                                مهدی ایلخانی نسب
+                            </h3>
+                            <p className={styles["real-tors__text"]}>869 فروش خانه</p>
+                        </div>
+
+                        <img
+                            src="/img/realtor-2.jpeg"
+                            alt="real-tors top 2"
+                            className={styles["real-tors__img"]}
+                        />
+                        <div className={styles["real-tors__details"]}>
+                            <h3 className={"u-heading-3  u-heading--white"}>کوثر بهشتی</h3>
+                            <p className={styles["real-tors__text"]}>243 فروش خانه</p>
+                        </div>
+
+                        <img
+                            src="/img/realtor-3.jpeg"
+                            alt="real-tors top 3"
+                            className={styles["real-tors__img"]}
+                        />
+                        <div className={styles["real-tors__details"]}>
+                            <h3 className={"u-heading-3  u-heading--white"}>
+                                عرشیا احسنی
+                            </h3>
+                            <p className={styles["real-tors__text"]}>130 فروش خانه</p>
+                        </div>
+                    </div>
+                </>
+            }
         </div>
     )
 }
